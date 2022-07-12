d9980a3ce3fad9a8cb7d3c47f7ee6e252a65d7a0
Jiwoncoding <seojiwonba@gmail.com>
2022-07-12 12:51:11 +0900

    가상 코인 거래소 화면 배치

--- coin-market/src/index.js
+++ coin-market/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+import App from './08/CoinApp';
+import './doit-ui/app.css';
+// import CoinApp from './03/TodoList';
+import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<CoinApp />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
